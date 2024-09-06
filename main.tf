02c649e7eb0204b8ed657286128bd93262e6a1e1
Avik Datta <avik.datta@imperial.ac.uk>
2024-09-06 10:43:55 +0100

    added ecr repo for demult

--- main.tf
+++ main.tf
@@ -111,3 +111,19 @@ module "efs-scratch" {
     }
   }
 }
+
+## ECR - demult
+resource "aws_ecr_repository" "igf-pipeline-ecr-demult" {
+  name                 = "${var.name}/demult"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = false
+  tags                 = local.tags
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+}
